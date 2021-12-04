0ba9c5a91871f8adc46f7af7a7b91c105ad94e28
hburkard <hburkard@live.unc.edu>
2021-12-04 16:51:28 -0500

    round 2

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -173,7 +173,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-    return everyEven(arr, num => num%2 == 1);
+    return everyEven(arr, num => num%2 === 1);
 };
 
 
