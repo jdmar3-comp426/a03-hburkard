e24699a77ea2331daeb1c4dca04620cc901318da
hburkard <hburkard@live.unc.edu>
2021-12-04 12:32:03 -0500

    fixing medium_2.js

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -23,7 +23,7 @@ see under the methods section
 var hycount = 0;
 
 for (let i=0; i < mpg_data.length; i++){
-    if (mpg_data[i].hybrid == True){
+    if (mpg_data[i].hybrid){
         hycount++
     }
 }
